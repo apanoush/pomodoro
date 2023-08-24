5c84066ca8dc5233e0fea48a84ab8879ffc309a2
anoush <dakeon@outlook.fr>
2023-08-24 23:17:11 +0200

    argument update

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,33 @@ use std::thread::sleep;
 use std::time::Duration;
 use std::io::Write;
 use chrono::{self, Timelike};
+use std::env;
 
 fn main() {
-    
+
+    let args: Vec<String> = env::args().collect();
+
+    let waiting_time: i64 = ( if args.len() > 1 { 
+        match args[1].parse::<i64>() {
+            Ok(x) => if x > 0 {Ok(x)} else {panic!("Usage: {} ARG, ARG must be a positive integer", args[0])},
+            Err(e) => Err(e)
+        }
+    } else {Ok(20)} ).expect("error in the argument, must be a positive integer");
+  
     // counters for seconds and minutes
-    let mut min: u32 = 0;
-    let mut sec: u32 = 0;
+    let mut min: i64 = 0;
+    let mut sec: i64 = 0;
 
     // duration of a second
     let second: Duration = Duration::from_secs(1);
 
     // getting the local time and adding 20mins to get the finishing time
     let current_time = chrono::Local::now();
-    let finish_time = current_time.add(chrono::Duration::minutes(20));
+    let finish_time = current_time.add(chrono::Duration::minutes(waiting_time));
     // printing them
     println!("current time: {:02}:{:02}, finish time: {:02}:{:02}", current_time.hour(), current_time.minute(), finish_time.hour(), finish_time.minute());
 
-    while min < 20 {
+    while min < waiting_time {
 
         print!("\r{:02}:{:02}", min, sec);
         std::io::stdout().flush().expect("flush error");
@@ -32,4 +42,5 @@ fn main() {
             min += 1;
         }
     }
+    println!("\nwell done!!:)")
 }
