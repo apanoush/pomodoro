fb204018ff62baa10b57382d4723323c7340e01f
anoush <dakeon@outlook.fr>
2023-08-24 18:03:52 +0200

    test

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+use std::ops::Add;
 use std::thread::sleep;
 use std::time::Duration;
 use std::io::Write;
+use std::time::SystemTime;
 
 fn main() {
     
@@ -8,6 +10,11 @@ fn main() {
     let mut sec: u32 = 0;
 
     let second: Duration = Duration::from_secs(1);
+
+    let current_time = SystemTime::now();
+    let finish_time = current_time.add(Duration::from_secs_f32(200.0));
+
+    println!("current time: {:?}, finish_time: {:?}", current_time, finish_time);
 
     while min < 20 {
         print!("\r{:02}:{:02}", min, sec);
